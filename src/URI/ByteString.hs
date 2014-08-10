87d7b3fdfc7f211a8501e668440a38f43a043b41
Michael Xavier <michael@michaelxavier.net>
2014-08-09 22:08:43 -0700

    Documentation

--- src/URI/ByteString.hs
+++ src/URI/ByteString.hs
@@ -4,7 +4,19 @@
 {-# LANGUAGE NoMonomorphismRestriction  #-}
 {-# LANGUAGE OverloadedStrings          #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
+{-|
+Module      : URI.ByteString
+Description : ByteString URI Parser
+Copyright   : (c) Soostone Inc., 2014
+                  Michael Xavier, 2014
+License     : GPL-3
+Maintainer  : michael@michaelxavier.net
+Stability   : experimental
+Portability : POSIX
 
+URI.ByteString aims to be an RFC3986 compliant URI parser that uses
+efficient ByteStrings for parsing and representing the data.
+|-}
 module URI.ByteString (
                       -- URI-related types
                         Scheme(..)
@@ -114,8 +126,10 @@ instance NFData URIParseError
 parseUri :: ByteString -> Either URIParseError URI
 parseUri = parseOnly' OtherError uriParser
 
+-- | Convenience alias for a parser that can return URIParseError
 type URIParser = Parser' URIParseError
 
+-- | Toplevel parser for URIs
 uriParser :: URIParser URI
 uriParser = do
   scheme <- schemeParser
@@ -126,6 +140,7 @@ uriParser = do
   frag  <- mFragmentParser
   return $ URI scheme authority path query frag
 
+-- | Parser for scheme, e.g. "http", "https", etc.
 schemeParser :: URIParser Scheme
 schemeParser = do
   c    <- satisfy isAlpha           `orFailWith` MalformedScheme NonAlphaLeading
@@ -135,24 +150,40 @@ schemeParser = do
     isSchemeValid = inClass $ "-+." ++ alphaNum
 
 --TODO: handle absolute, noscheme, empty
+
+-- | Heir part immediately follows the schema and encompasses the
+-- authority and path sections.
 heirPartParser :: URIParser (Maybe Authority, ByteString)
 heirPartParser = authWithPathParser <|>
                  pathAbsoluteParser <|>
                  pathRootlessParser <|>
                  pathEmptyParser
 
+-- | See the "authority path-abempty" grammar in the RFC
 authWithPathParser :: URIParser (Maybe Authority, ByteString)
 authWithPathParser = string' "//" *> ((,) <$> mAuthorityParser <*> pathParser)
 
+-- | See the "path-absolute" grammar in the RFC. Essentially a special
+-- case of rootless.
 pathAbsoluteParser :: URIParser (Maybe Authority, ByteString)
 pathAbsoluteParser = string' "/" *> pathRootlessParser
 
+-- | See the "path-rootless" grammar in the RFC.
 pathRootlessParser :: URIParser (Maybe Authority, ByteString)
 pathRootlessParser = (,) <$> pure Nothing <*> pathParser1
 
+-- | See the "path-empty" grammar in the RFC. Must not be followed
+-- with a path-valid char.
 pathEmptyParser :: URIParser (Maybe Authority, ByteString)
-pathEmptyParser = undefined
+pathEmptyParser = do
+  nextChar <- peekWord8 `orFailWith` OtherError "impossible peekWord8 error"
+  case nextChar of
+    Just c -> guard (notInClass pchar c) >> return emptyCase
+    _      -> return emptyCase
+  where
+    emptyCase = (Nothing, mempty)
 
+-- | Parser whe
 mAuthorityParser :: URIParser (Maybe Authority)
 mAuthorityParser = mParse authorityParser
 
@@ -170,22 +201,26 @@ userInfoParser =  (uiTokenParser <* word8 atSym) `orFailWith` MalformedUserInfo
       return $ UserInfo user pass
     validForUserInfo = inClass $ unreserved ++ pctEncoded ++ subDelims ++ ":"
 
+-- | Authority consists of host and port
 authorityParser :: URIParser Authority
 authorityParser = Authority <$> mParse userInfoParser <*> hostParser <*> mPortParser
 
---FIXME: without tld validation, invalid ip addresses pass through
+-- | Parser that can handle IPV6/Future literals, IPV4, and domain names.
 hostParser :: URIParser Host
 hostParser = (Host <$> parsers) `orFailWith` MalformedHost
   where
     parsers = ipLiteralParser <|> ipV4Parser <|> regNameParser
     ipLiteralParser = word8 oBracket *> (ipV6Parser <|> ipVFutureParser) <* word8 cBracket
 
+-- | Parses IPV6 addresses. See relevant section in RFC.
 ipV6Parser :: Parser ByteString
 ipV6Parser = fail "FIXME"
 
+-- | Parses IPVFuture addresses. See relevant section in RFC.
 ipVFutureParser :: Parser ByteString
 ipVFutureParser = fail "FIXME"
 
+-- | Parses a valid IPV4 address
 ipV4Parser :: Parser ByteString
 ipV4Parser = mconcat <$> sequence [ decOctet
                                   , dot
@@ -204,20 +239,26 @@ ipV4Parser = mconcat <$> sequence [ decOctet
       return s
     dot = string "."
 
+-- | This corresponds to the hostname, e.g. www.example.org
 regNameParser :: Parser ByteString
 regNameParser = urlDecode' <$> A.takeWhile1 (inClass validForRegName)
   where
     validForRegName = unreserved ++ pctEncoded ++ subDelims
 
+-- | Only parse a port if the colon signifier is there.
 mPortParser :: URIParser (Maybe Port)
 mPortParser = word8' colon `thenJust` portParser
 
+-- | Parses port number from the hostname. Colon separator must be
+-- handled elsewhere.
 portParser :: URIParser Port
 portParser = (Port <$> A.takeWhile1 isDigit) `orFailWith` MalformedPort
 
+-- | Path with any number of segments
 pathParser :: URIParser ByteString
 pathParser = pathParser' A.many'
 
+-- | Path with at least 1 segment
 pathParser1 :: URIParser ByteString
 pathParser1 = pathParser' A.many1'
 
@@ -228,7 +269,11 @@ pathParser' repeatParser = (mconcat <$> repeatParser segmentParser) `orFailWith`
   where
     segmentParser = mconcat <$> sequence [string "/", A.takeWhile (inClass pchar)]
 
--- | This parser is being a bit pragmatic. The query section in the spec does not identify the key/value format used in URIs
+-- | This parser is being a bit pragmatic. The query section in the
+-- spec does not identify the key/value format used in URIs, but that
+-- is what most users are expecting to see. One alternative could be
+-- to just expose the query string as a string and offer functions on
+-- URI to parse a query string to a Query.
 queryParser :: URIParser Query
 queryParser = do
   mc <- peekWord8 `orFailWith` OtherError "impossible peekWord8 error"
@@ -240,6 +285,9 @@ queryParser = do
   where
     itemsParser = Query <$> A.sepBy' queryItemParser (word8' ampersand)
 
+-- | When parsing a single query item string like "foo=bar", turns it
+-- into a key/value pair as per convention, with the value being
+-- optional. & separators need to be handled further up.
 queryItemParser :: URIParser (ByteString, Maybe ByteString)
 queryItemParser = do
   s <- A.takeWhile1 validForQuery `orFailWith` MalformedQuery
@@ -251,14 +299,17 @@ queryItemParser = do
   where
     validForQuery = inClass ('?':'/':delete '&' pchar)
 
+-- | Only parses a fragment if the # signifiier is there
 mFragmentParser :: URIParser (Maybe ByteString)
 mFragmentParser = word8' hash `thenJust` fragmentParser
   where
     hash = 35
 
 -- TODO: may want to just take till EOS and then check and see if its valid
+
+-- | The final piece of a uri, e.g. #fragment, minus the #.
 fragmentParser :: URIParser ByteString
-fragmentParser = A.takeWhile validFragmentWord `orFailWith` MalformedFragment
+fragmentParser = A.takeWhile1 validFragmentWord `orFailWith` MalformedFragment
   where
     validFragmentWord = inClass ('?':'/':pchar)
 
@@ -320,17 +371,24 @@ ampersand = 38
           ---------------------------
 
 -- FIXME: theres probably a much better way to do this
+
+-- | Convert a bytestring into an int representation. Assumes the
+-- entire string is comprised of 0-9 digits.
 bsToNum :: ByteString -> Int
 bsToNum s = sum $ zipWith (*) (reverse ints) [10 ^ x | x <- [0..] :: [Int]]
   where
     w2i w = fromIntegral $ w - 48
     ints  = map w2i . BS.unpack $ s
 
+-- | Decoding specifically for the query string, which decodes + as
+-- space.
 urlDecodeQuery :: ByteString -> ByteString
 urlDecodeQuery = urlDecode plusToSpace
   where
     plusToSpace = True
 
+-- | Decode any part of the URL besides the query, which decodes + as
+-- space.
 urlDecode' :: ByteString -> ByteString
 urlDecode' = urlDecode plusToSpace
   where
@@ -339,6 +397,11 @@ urlDecode' = urlDecode plusToSpace
           ----------------------------------------
           -- Parsing with Strongly-Typed Errors --
           ----------------------------------------
+-- | A parser with a specific error type. Attoparsec unfortunately
+-- throws all errors into strings, which cannot be handled well
+-- programmatically without doing something silly like parsing error
+-- messages. This wrapper attempts to concentrate these errors into
+-- one type.
 newtype Parser' e a = Parser' (Parser a)
                     deriving ( Functor
                              , Applicative
@@ -347,29 +410,48 @@ newtype Parser' e a = Parser' (Parser a)
                              , MonadPlus
                              , Monoid)
 
+-- | Use with caution. Catch a parser failing and return Nothing.
 mParse :: Parser' e a -> Parser' e (Maybe a)
 mParse p = option Nothing (Just <$> p)
 
+-- | If the first parser succeeds, discard the result and use the
+-- second parser (which may fail). If the first parser fails, return
+-- Nothing. This is used to check a benign precondition that indicates
+-- the presence of a parsible token, i.e. ? preceeding a query.
 thenJust :: Parser' e a -> Parser' e b -> Parser' e (Maybe b)
 thenJust p1 p2 = p1 *> (Just <$> p2) <|> pure Nothing
 
+-- | Lift a word8 Parser into a strongly error typed parser. This will
+-- generate a "stringy" error message if it fails, so you should
+-- probably be prepared to exit with a nicer error further up.
 word8' :: Word8 -> Parser' e Word8
 word8' = Parser' . word8
 
+-- | Skip exactly 1 character. Fails if the character isn't
+-- there. Generates a "stringy" error.
 skip' :: Int -> Parser' e ()
 skip' = Parser' . void . A.take
 
-
+-- | Lifted version of the string token parser. Same caveats about
+-- "stringy" errors apply.
 string' :: ByteString -> Parser' e ByteString
 string' = Parser' . string
 
+-- | Combinator for tunnelling more specific error types through the
+-- attoparsec machinery using read/show.
 orFailWith :: (Show e, Read e) => Parser a -> e -> Parser' e a
 orFailWith p e = Parser' p <|> fail' e
 
+-- | Should be preferred to fail'
 fail' :: (Show e, Read e) => e -> Parser' e a
 fail' = fail . show
 
-parseOnly' :: (Read e, Show e) => (String -> e) -> Parser' e a -> ByteString -> Either e a
+-- | Stronger-typed variation of parseOnly'. Consumes all input.
+parseOnly' :: (Read e, Show e)
+              => (String -> e) -- ^ Fallback if we can't parse a failure message for the sake of totality.
+              -> Parser' e a
+              -> ByteString
+              -> Either e a
 parseOnly' noParse (Parser' p) = fmapL readWithFallback . parseOnly p
   where
     readWithFallback s = fromMaybe (noParse s) (readMaybe . stripAttoparsecGarbage $ s)
@@ -381,5 +463,6 @@ parseOnly' noParse (Parser' p) = fmapL readWithFallback . parseOnly p
 stripAttoparsecGarbage :: String -> String
 stripAttoparsecGarbage = stripPrefix' "Failed reading: "
 
+-- | stripPrefix where it is a noop if the prefix doesn't exist.
 stripPrefix' :: Eq a => [a] -> [a] -> [a]
 stripPrefix' pfx s = fromMaybe s $ stripPrefix pfx s
