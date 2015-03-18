585f1a2243d92c0fcd7156d5c6f67960db718d55
Michael Xavier <michael@michaelxavier.net>
2015-03-18 11:34:53 -0700

    Bench against network-uri

--- bench/Main.hs
+++ bench/Main.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
 module Main (main) where
 
 -------------------------------------------------------------------------------
-import           Criterion.Main
 import           Control.DeepSeq.Generics
+import           Criterion.Main
+import           Data.String
+import qualified Network.URI                as NU
 -------------------------------------------------------------------------------
 import           URI.ByteString
 -------------------------------------------------------------------------------
@@ -12,12 +15,20 @@ import           URI.ByteString
 instance NFData Authority
 instance NFData UserInfo
 instance NFData URI
+instance NFData NU.URI
 instance NFData SchemaError
 instance NFData URIParseError
 
 
 -------------------------------------------------------------------------------
 main :: IO ()
-main = defaultMain [
-  bench "parse" $ nf (parseURI strictURIParserOptions) "http://google.com/example?params=youbetcha"
-                   ]
+main = defaultMain
+  [
+    bench "Network.URI.parseURI" $ nf NU.parseURI exampleUri
+  , bench "URI.ByteString.parseURI strict" $ nf (parseURI strictURIParserOptions) exampleUri
+  , bench "URI.ByteString.parseURI lax" $ nf (parseURI laxURIParserOptions) exampleUri
+  ]
+
+
+exampleUri :: IsString s => s
+exampleUri = "http://google.com/example?params=youbetcha"
