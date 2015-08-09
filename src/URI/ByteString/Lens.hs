b3ffc170d574de26350c860f3c02de1f3e8c3f32
Michael Xavier <michael@michaelxavier.net>
2015-08-08 21:09:36 -0700

    Do the unexported lens alias trick
    This resolves #17

--- src/URI/ByteString/Lens.hs
+++ src/URI/ByteString/Lens.hs
@@ -13,213 +13,141 @@ import           URI.ByteString.Types
 -------------------------------------------------------------------------------
 
 
--- | @
--- schemeBSL :: Lens' 'Scheme' 'ByteString'
--- @
+-------------------------------------------------------------------------------
+
+-------------------------------------------------------------------------------
 schemeBSL
-  :: Functor f => (ByteString -> f ByteString) -> Scheme -> f Scheme
+  :: Lens' Scheme ByteString
 schemeBSL =
   lens schemeBS (\a b -> a { schemeBS = b})
 {-# INLINE schemeBSL #-}
 
 -------------------------------------------------------------------------------
--- | @
--- hostBSL :: Lens' 'Host' 'ByteString'
--- @
 hostBSL
-  :: Functor f => (ByteString -> f ByteString) -> Host -> f Host
+  :: Lens' Host ByteString
 hostBSL =
   lens hostBS (\a b -> a { hostBS = b})
 {-# INLINE hostBSL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- portNumberL :: Lens' 'Port' 'Int'
--- @
 portNumberL
-  :: Functor f => (Int -> f Int) -> Port -> f Port
+  :: Lens' Port Int
 portNumberL =
   lens portNumber (\a b -> a { portNumber = b})
 {-# INLINE portNumberL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- authorityUserInfoL :: Lens' 'Authority' ('Maybe' 'UserInfo')
--- @
 authorityUserInfoL
-  :: Functor f =>
-     (Maybe UserInfo -> f (Maybe UserInfo)) -> Authority -> f Authority
+  :: Lens' Authority (Maybe UserInfo)
 authorityUserInfoL =
   lens authorityUserInfo (\a b -> a { authorityUserInfo = b})
 {-# INLINE authorityUserInfoL #-}
 
 -------------------------------------------------------------------------------
--- | @
--- authorityHostL :: Lens' 'Authority' 'Host'
--- @
 authorityHostL
-  :: Functor f => (Host -> f Host) -> Authority -> f Authority
+  :: Lens' Authority Host
 authorityHostL =
   lens authorityHost (\a b -> a { authorityHost = b})
 {-# INLINE authorityHostL #-}
 
 -------------------------------------------------------------------------------
--- | @
--- authorityPortL :: Lens' 'Authority' ('Maybe' 'Port')
--- @
 authorityPortL
-  :: Functor f =>
-     (Maybe Port -> f (Maybe Port)) -> Authority -> f Authority
+  :: Lens' Authority (Maybe Port)
 authorityPortL =
   lens authorityPort (\a b -> a { authorityPort = b})
 {-# INLINE authorityPortL #-}
 
 -------------------------------------------------------------------------------
--- | @
--- uiUsernameL :: Lens' 'UserInfo' 'ByteString'
--- @
 uiUsernameL
-  :: Functor f =>
-     (ByteString -> f ByteString) -> UserInfo -> f UserInfo
+  :: Lens' UserInfo ByteString
 uiUsernameL =
   lens uiUsername (\a b -> a { uiUsername = b})
 {-# INLINE uiUsernameL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- uiPasswordL :: Lens' 'UserInfo' 'ByteString'
--- @
 uiPasswordL
-  :: Functor f =>
-     (ByteString -> f ByteString) -> UserInfo -> f UserInfo
+  :: Lens' UserInfo ByteString
 uiPasswordL =
   lens uiPassword (\a b -> a { uiPassword = b})
 {-# INLINE uiPasswordL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- queryPairsL :: Lens' 'Query' [('ByteString', 'ByteString')]
--- @
 queryPairsL
-  :: Functor f
-  => ([(ByteString, ByteString)] -> f [(ByteString, ByteString)])
-  -> Query
-  -> f Query
+  :: Lens' Query [(ByteString, ByteString)]
 queryPairsL =
   lens queryPairs (\a b -> a { queryPairs = b})
 {-# INLINE queryPairsL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- uriSchemeL :: Lens' 'URI' 'Scheme'
--- @
-uriSchemeL :: Functor f => (Scheme -> f Scheme) -> URI -> f URI
+uriSchemeL :: Lens' URI Scheme
 uriSchemeL =
   lens uriScheme (\a b -> a { uriScheme = b})
 {-# INLINE uriSchemeL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- uriAuthorityL :: Lens' 'URI' ('Maybe' 'Authority')
--- @
-uriAuthorityL
-  :: Functor f =>
-     (Maybe Authority -> f (Maybe Authority)) -> URI -> f URI
+uriAuthorityL :: Lens' URI (Maybe Authority)
 uriAuthorityL =
   lens uriAuthority (\a b -> a { uriAuthority = b})
 {-# INLINE uriAuthorityL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- uriPathL :: Lens' 'URI' 'ByteString'
--- @
-uriPathL
-  :: Functor f => (ByteString -> f ByteString) -> URI -> f URI
+uriPathL :: Lens' URI ByteString
 uriPathL =
   lens uriPath (\a b -> a { uriPath = b})
 {-# INLINE uriPathL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- uriQueryL :: Lens' 'URI' 'Query'
--- @
-uriQueryL :: Functor f => (Query -> f Query) -> URI -> f URI
+uriQueryL :: Lens' URI Query
 uriQueryL =
   lens uriQuery (\a b -> a { uriQuery = b})
 {-# INLINE uriQueryL #-}
 
+
 -------------------------------------------------------------------------------
--- | @
--- uriFragmentL :: Lens' 'URI' ('Maybe' 'ByteString')
--- @
-uriFragmentL
-  :: Functor f =>
-     (Maybe ByteString -> f (Maybe ByteString)) -> URI -> f URI
+uriFragmentL :: Lens' URI (Maybe ByteString)
 uriFragmentL =
   lens uriFragment (\a b -> a { uriFragment = b})
 {-# INLINE uriFragmentL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- rrAuthorityL :: Lens' 'RelativeRef' ('Maybe' 'Authority')
--- @
-rrAuthorityL
-  :: Functor f =>
-     (Maybe Authority -> f (Maybe Authority)) -> RelativeRef -> f RelativeRef
+rrAuthorityL :: Lens' RelativeRef (Maybe Authority)
 rrAuthorityL =
   lens rrAuthority (\a b -> a { rrAuthority = b})
 {-# INLINE rrAuthorityL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- rrPathL :: Lens' 'RelativeRef' 'ByteString'
--- @
-rrPathL
-  :: Functor f => (ByteString -> f ByteString) -> RelativeRef -> f RelativeRef
+rrPathL :: Lens' RelativeRef ByteString
 rrPathL =
   lens rrPath (\a b -> a { rrPath = b})
 {-# INLINE rrPathL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- rrQueryL :: Lens' 'RelativeRef' 'Query'
--- @
-rrQueryL :: Functor f => (Query -> f Query) -> RelativeRef -> f RelativeRef
+rrQueryL :: Lens' RelativeRef Query
 rrQueryL =
   lens rrQuery (\a b -> a { rrQuery = b})
 {-# INLINE rrQueryL #-}
 
+
 -------------------------------------------------------------------------------
--- | @
--- rrFragmentL :: Lens' 'RelativeRef' ('Maybe' 'ByteString')
--- @
-rrFragmentL
-  :: Functor f =>
-     (Maybe ByteString -> f (Maybe ByteString)) -> RelativeRef -> f RelativeRef
+rrFragmentL :: Lens' RelativeRef (Maybe ByteString)
 rrFragmentL =
   lens rrFragment (\a b -> a { rrFragment = b})
 {-# INLINE rrFragmentL #-}
 
 
 -------------------------------------------------------------------------------
--- | @
--- upoValidQueryCharL :: Lens' URIParserOptions (Word8 -> Bool)
--- @
-upoValidQueryCharL
-  :: Functor f =>
-     ((Word8 -> Bool) -> f (Word8 -> Bool))
-     -> URIParserOptions -> f URIParserOptions
+upoValidQueryCharL :: Lens' URIParserOptions (Word8 -> Bool)
 upoValidQueryCharL =
   lens upoValidQueryChar (\a b -> a { upoValidQueryChar = b})
 {-# INLINE upoValidQueryCharL #-}
@@ -228,7 +156,11 @@ upoValidQueryCharL =
 -------------------------------------------------------------------------------
 -- Lens machinery
 -------------------------------------------------------------------------------
+-- Unexported type aliases to clean up the documentation
 type Lens s t a b = forall f. Functor f => (a -> f b) -> s -> f t
+
+type Lens' s a = Lens s s a a
+
 
 -------------------------------------------------------------------------------
 lens :: (s -> a) -> (s -> b -> t) -> Lens s t a b
